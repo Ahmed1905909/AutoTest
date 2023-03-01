dee52995566a723a6b84566d6f6e24309928ef03
Omarashry99 <omar1909833@miuegypt.edu.eg>
2023-03-02 01:18:31 +0200

    update views for api

--- webapp/web_project/AutoTest/views.py
+++ webapp/web_project/AutoTest/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from nlpcloud import Client
+
 def AutoTest(request):
     template = loader.get_template('home.html')
     return HttpResponse(template.render())
+client = Client("fast-gpt-j", "", gpu=True)
+
+@csrf_exempt
+def generate_code(request):
+    if request.method == 'POST':
+        text = request.POST.get('text')
+        response = client.code_generation(text)
+        return JsonResponse({'generated_code': response['generated_code']})
+    else:
+        return JsonResponse({'error': 'Invalid request method'})
+
