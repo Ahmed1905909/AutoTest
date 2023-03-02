5ff0a8c70eef014916b3b5c5ca839e23be7c81a4
miriamayman <97457782+miriamayman@users.noreply.github.com>
2023-03-02 02:07:01 +0200

    edit1

--- webapp/web_project/AutoTest/urls.py
+++ webapp/web_project/AutoTest/urls.py
@@ -4,4 +4,5 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('AutoTest/', views.AutoTest, name='AutoTest'),
 ]
+
 urlpatterns += staticfiles_urlpatterns()
--- webapp/web_project/AutoTest/views.py
+++ webapp/web_project/AutoTest/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.template import loader
+
+
+
 def AutoTest(request):
     template = loader.get_template('home.html')
     return HttpResponse(template.render())
